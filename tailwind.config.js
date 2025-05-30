d0746c687c3fc71377d225dccabc560becb12744
ZakhBen <benzakh56@gmail.com>
2025-05-30 13:52:34 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
+```css
+
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
+
+```
